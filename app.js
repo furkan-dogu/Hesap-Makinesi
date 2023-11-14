852476fa7ed6dcb4d60513b9e5d36bde323b7432
Furkan Doğu <furkandogu2018@gmail.com>
2023-11-14 12:35:18 +0300

    js düzenlendi

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ function topla() {
   let sayi1 = Number(document.getElementById("sayi1").value);
   let sayi2 = Number(document.getElementById("sayi2").value);
 
-  document.getElementById("sonuc").innerHTML = `Sonuç: ${sayi1 + sayi2}`;
+  document.getElementById("sonuc").textContent = `Sonuç: ${sayi1 + sayi2}`;
 }
 
 function cikar() {
   let sayi1 = Number(document.getElementById("sayi1").value);
   let sayi2 = Number(document.getElementById("sayi2").value);
 
-  document.getElementById("sonuc").innerHTML = `Sonuç: ${sayi1 - sayi2}`;
+  document.getElementById("sonuc").textContent = `Sonuç: ${sayi1 - sayi2}`;
 }
 
 function carp() {
   let sayi1 = Number(document.getElementById("sayi1").value);
   let sayi2 = Number(document.getElementById("sayi2").value);
 
-  document.getElementById("sonuc").innerHTML = `Sonuç: ${sayi1 * sayi2}`;
+  document.getElementById("sonuc").textContent = `Sonuç: ${sayi1 * sayi2}`;
 }
 
 function bol() {
@@ -24,8 +24,8 @@ function bol() {
   let sayi2 = Number(document.getElementById("sayi2").value);
 
   if (sayi2 !== 0) {
-    document.getElementById("sonuc").innerHTML = `Sonuç: ${sayi1 / sayi2}`;
+    document.getElementById("sonuc").textContent = `Sonuç: ${sayi1 / sayi2}`;
   } else {
-    document.getElementById("sonuc").innerHTML = `Sayı sıfıra bölünemez. Hatalı işlem yaptınız.`;
+    document.getElementById("sonuc").textContent = `Sayı sıfıra bölünemez. Hatalı işlem yaptınız.`;
   }
 }
